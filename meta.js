1817de7b85d70d7be8a69e0fb8e12964b7b54023
Rebaz Saleh <rss1803@rit.edu>
2017-05-14 21:14:53 -0400

    change category to list

--- meta.js
+++ meta.js
@@ -25,7 +25,7 @@ module.exports = {
       "message": "Author"
     },
     "Category": {
-      "type": "string",
+      "type": "list",
       "message": "Category",
       "choices": [
         {
